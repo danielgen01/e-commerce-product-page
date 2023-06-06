80542bad2f26770bb435a2228bbd3c6303f7cd11
Daniel <gendik38@gmail.com>
2023-06-06 17:42:18 +0200

    desktop layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
 import Header from './components/Header'
 import ImageCarousel from './components/ImageCarousel'
-import Main from './components/Caption'
+import Caption from './components/Caption'
 
 function App() {
 
   return (
     <div className='overflow-x-hidden'>
       <Header />
+
+      <div className="layout grid grid-cols-1 lg:grid-cols-2 lg:mt-20">
       <ImageCarousel />
-      <Main />
+      <Caption />
+      </div>
   
     </div>
   )
--- src/components/Caption.tsx
+++ src/components/Caption.tsx
@@ -17,26 +17,31 @@ const Caption:React.FC = () => {
   };
 
   return (
-    <figcaption className="px-10 py-5 flex flex-col gap-4 caption">
-      <h1 className="text-Orange/80 font-bold tracking-wider text-lg">
+    <figcaption className="px-10 py-5 flex flex-col gap-4 caption lg:gap-7">
+      <h1 className="text-Orange/80 font-bold tracking-wider text-lg lg:text-xl">
         Sneaker Company
       </h1>
-      <h2 className="font-bold text-4xl leading-10 text-left ">
+      <h2 className="font-bold text-4xl leading-10 text-left lg:text-5xl">
         Fall Limited Edtion Sneakers
       </h2>
-      <p className="text-Dark-grayish-blue font-medium text-lg">
+      <p className="text-Dark-grayish-blue font-medium text-md lg:text-2xl">
         These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they'll withstand everything the
         weather can offer.
       </p>
-      <div className="price flex items-center gap-5">
+      <div className="price">
+        <div className="price-and-discount flex flex-row items-center gap-5">
         <h3 className="font-bold text-3xl">$125.00</h3>
         <span className="bg-Pale-orange px-3 py-1 rounded-md text-Orange font-bold">
           50%
         </span>
         <s className="ml-auto text-Grayish-blue font-bold  text-xl">$250.00</s>
+        </div>
+        
+       
       </div>
 
+<section className="quantity-box-and-addtocart-btn grid grid-cols-1 lg:grid-cols-2">
       <div className="quantity-box bg-Light-grayish-blue w-full h-14 rounded-lg">
         <div className="quantity-box-content px-4 flex justify-between h-full w-full items-center">
           <button onClick={decrementQuantity} className="h-full">
@@ -49,10 +54,14 @@ const Caption:React.FC = () => {
         </div>
       </div>
 
-      <button className="add-to-cart-button h-16 bg-Orange overflow-x-hidden rounded-xl flex justify-center items-center gap-4 text-white font-bold" >
-        <img src="public\assets\images\icon-cart.svg" alt="" />
-        <p>Add to cart</p>
-      </button>
+      <div className="addtocart h-16 w-full ">
+  <button className="add-to-cart-button bg-Orange w-full rounded-xl flex justify-center items-center gap-4 text-white font-bold hover:opacity-75 duration-150 h-full">
+    <img src="public/assets/images/icon-cart.svg" alt="cart-icon" />
+    <p className="text-lg">Add to cart</p>
+  </button>
+</div>
+</section>
+
     </figcaption>
   )
 }
